e8a7c9531559d1069f636972bdf54693c3d455ba
Nicholas Jones <njones314@outlook.com>
2023-09-30 13:03:48 -0600

    Updating auto_start script to properly reset the FBI command between loops

--- sdo_auto_start.sh
+++ sdo_auto_start.sh
@@ -1,42 +1,45 @@
 #! /bin/bash
 cd "$(dirname "$0")"
 
-# Set the backlight on the Hyperpixel screen
-sudo pigpiod
-pigs hp 19 100 50000
-
 #SDO Website URL
 LATEST_URL=https://sdo.gsfc.nasa.gov/assets/img/latest
 
 # DOWNLOAD PATH
 LOCALDIR=../IMG_DOWNLOAD
 
-MODES=( 0 1 )
+# Channel selection
 CHANNELS=( 0094 0131 0171 0193 0211 0304 0335 1600 1700 HMIB HMII HMID HMIBC HMIIF HMIIC )
-RESOLUTIONS=( 4096 2048 1024 512 )
 
-# Mode selection. Select operating mode
-val_mod=false
+# Resolution selection (4096, 2048, 1024, 512)
+RES_SEL=512
 
-res_sel=512
+# Set image blend time for FBI command, in ms
+B_TIME=500
+
+# Set the backlight brightness
+B_LIGHT=50000
+
+# Set the backlight on the Hyperpixel screen
+sudo pigpiod
+pigs hp 19 100 $B_LIGHT
 
 # ALL CHANNEL DYNAMIC
-# Infinite loop to continue grabbing images from SDO database
+# Infinite loop to continue grabbing images from SDO database every 15 minutes
 while :
 do
 	for CHANNEL in ${CHANNELS[@]}
 	do
-		FILENAME="latest_"$res_sel"_"$CHANNEL".jpg"
+		FILENAME="latest_"$RES_SEL"_"$CHANNEL".jpg"
 		URL=$LATEST_URL/$FILENAME
 
 		# wget command
 		wget -N -nd --no-check-certificate --inet4-only $URL --directory-prefix=$LOCALDIR
 	done
 
-	# Reset tty2
-	echo Q^v^o > /dev/tty2
+	# kill the previous FBI command
+	sudo pkill -SIGKILL -t tty2
 
-	sudo fbi -a -T 2 -noverbose -t 30 $LOCALDIR/*
+	sudo fbi -a -T 2 -noverbose -t 30 --blend $B_TIME $LOCALDIR/*
 	echo "LOOP COMPLETE"
 	sleep 15m
 done
